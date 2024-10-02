8bc0c5f09efa85cf8e08572b0ffab7563bd13b05
Shu Yi Kang <185901261@qq.com>
2024-10-02 23:42:26 +0800

    chore(all): [build] init

---
+++ src/extension.ts
@@ -0,0 +1,51 @@
+import * as vscode from "vscode"
+import fs from "fs"
+export function activate(context: vscode.ExtensionContext) {
+    console.log("插件已经被激活")
+
+    let disposable = vscode.commands.registerCommand("extension.extractCssClasses", async () => {
+        const editor = vscode.window.activeTextEditor
+
+        if (!editor || editor.document.languageId !== "typescriptreact") {
+            vscode.window.showWarningMessage("请打开一个tsx的文件")
+            return
+        }
+
+        const documentText = editor.document.getText()
+        const classNames = extractClassNames(documentText)
+
+        if (classNames.length === 0) {
+            vscode.window.showInformationMessage("No class names found.")
+            return
+        }
+
+        const lessContent = generateLessContent(classNames)
+        const lessFileName = editor.document.fileName.replace(/\.tsx$/, ".less")
+
+        await vscode.workspace.fs.writeFile(vscode.Uri.file(lessFileName), Buffer.from(lessContent, "utf8"))
+        vscode.window.showInformationMessage(`LESS file created: ${lessFileName}`)
+    })
+
+    context.subscriptions.push(disposable)
+}
+
+function extractClassNames(text: string): string[] {
+    // const classRegex = /className="([^"]*)"/g
+    const regex = /className\s*=\s*{\s*styles\.(\w+)\s*}/g // 匹配 className={styles.xxx} 的正则表达式
+
+    const classNames: Set<string> = new Set()
+    let match
+
+    while ((match = regex.exec(text)) !== null) {
+        match[1].split(" ").forEach((cls) => classNames.add(cls))
+    }
+
+    return Array.from(classNames)
+}
+
+/** 生成less文件的内容 */
+function generateLessContent(classNames: string[]): string {
+    return classNames.map((cls) => `.${cls} {\n  // Add styles for ${cls}\n}`).join("\n\n")
+}
+// This method is called when your extension is deactivated
+export function deactivate() {}
